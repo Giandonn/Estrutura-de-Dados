66e27e3bebe0a53e48bdd94f0a473bc759b5022b
Giandonn <lucas_raineri04@hotmail.com>
2024-03-01 21:58:01 -0300

    upt

--- maistestes.py
+++ maistestes.py
@@ -2,4 +2,10 @@ import funcao_pesquisa_ordenacao
 from time import perf_counter
 
 lista = funcao_pesquisa_ordenacao.gera_lista_aleatoria(10, 1, 100)
+print("lista gerada")
+print(lista)
+
+print("ordenando a lista")
+lista.sort()
+print("lista ordenada")
 print(lista)
