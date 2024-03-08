8257375199490e7160d5762723226fc5fa8c97ec
Giandonn <lucas_raineri04@hotmail.com>
2024-03-08 19:15:00 -0300

    upt

--- pesquisa_ordenacao.py
+++ pesquisa_ordenacao.py
@@ -1,11 +1,11 @@
 from time import perf_counter
 
 inicio = perf_counter()
+
 #inicio do algoritmo
 acumulador = 0
 for i in range(10000000):
     acumulador += 1
-
 
 #fim do algoritmo
 fim = perf_counter()
