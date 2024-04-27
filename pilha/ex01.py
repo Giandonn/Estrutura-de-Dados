959ae5a707df684b39c8ee612c15dc9420b2c7a0
Giandonn <lucas_raineri04@hotmail.com>
2024-04-26 22:17:00 -0300

    introducao a pilhas

---
+++ pilha/ex01.py
@@ -0,0 +1,6 @@
+pilha = []
+
+if len(pilha) == 0:
+    print("a pilha esta vazia")
+else:
+    print("a pilha nao esta vazia")
---
+++ pilha/ex02.py
@@ -0,0 +1,12 @@
+pilha = []
+pilha.append(5)
+pilha.append(10)
+pilha.append(15)
+print(pilha)
+
+while len(pilha) > 0:
+    elemento = pilha.pop()
+    print(f"{elemento}: foi deletado da pilha")
+
+print(pilha)
+
