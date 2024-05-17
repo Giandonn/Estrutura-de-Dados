5eb227d29e4cc09727991c26ba004ce727b62715
Giandonn <lucas_raineri04@hotmail.com>
2024-05-17 20:05:04 -0300

    exemplo sobreescrever tupla

--- tuplas/exemploTupla.py
+++ tuplas/exemploTupla.py
@@ -3,3 +3,9 @@ tupla = (0, [1,2,3], (4,5,6), 7.0)
 print(f"{tupla[1]}, tipo da variavel {type(tupla)}")
 
 print(f"exemplo fatiamento: {tupla[:3]}")
+
+#sobreescrevendo tupla
+t1 = (1,2,3)
+t2 = (4,5,6)
+t1 = t1 + t2
+print(t1)
