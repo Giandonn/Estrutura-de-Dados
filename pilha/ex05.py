2c9e6316d58e0171228187fb63fc55b398fc32e0
Giandonn <lucas_raineri04@hotmail.com>
2024-05-17 19:38:56 -0300

    upt

--- pilha/ex05.py
+++ pilha/ex05.py
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-#undo redo
-
-menu = """
-1- realizar acao
-2- desfazer acao
-3- refazer acao
-4- imprimir pilhas
-5- sair
-"""
-
-desfazer = []
-refazer = []
-print(menu)
-opcao = input("escolha uma opcao")
-
-while opcao != 5:
-    match opcao:
-        case "1":
-            acao = input("informe a acao")
-            desfazer.append(acao)
-            print("lista desfazer")
-            imprimir_lista(desfazer)
-            print("Lista refazer")
-            imprimir_lista(refazer)
-        case "2":
-            acao = desfazer.pop()
-            refazer.append(acao)
-            print(f"acao {acao} desfeita")
-        case "3":
-            acao = refazer.pop()
-            desfazer.append(acao)
-            print(f"acao {acao} refeita")
-=======
 pilhaAcao = []
 pilhaAcaoDesfeita = []
 
@@ -58,4 +24,3 @@ for i in len(lista):
 		lista.pop()
 
 print(lista)
->>>>>>> 3c44eb71ea3c2749a241e5d844e29b51bbc77793
